5c8995b8ef52be2cef73d0b465977e751132dc7f
Alexander Karmanov <seym-melo@yandex.ru>
2023-12-18 17:01:17 +0400

    Added remove function

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
-const deleteButton = cardTemplate.querySelector('.card__delete-button');
 const likeButton = cardTemplate.querySelector('.card__like-button');
 
 // @todo: DOM узлы
@@ -10,15 +9,20 @@ let placesList = content.querySelector('.places__list');
 // @todo: Функция создания карточки
 function addCard(imageSource, titleValue) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
   cardElement.querySelector('.card__image').setAttribute('src', imageSource);
   cardElement.querySelector('.card__title').textContent = titleValue;
+  
+  deleteButton.addEventListener('click', removeCard) 
 
   placesList.append(cardElement);
 }
 
 // @todo: Функция удаления карточки
-
+function removeCard (element) {
+  element.target.parentNode.remove();
+}
 
 // @todo: Вывести карточки на страницу
 for (let i = 0; i < initialCards.length; i++) {
